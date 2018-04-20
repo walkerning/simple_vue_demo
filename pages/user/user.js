fcd6183975138e44f1526344156eb8d491c229a4
walkerning <foxdoraame@gmail.com>
2018-04-21 04:06:51 +0800

    Small chnage: Change the cover-view size

---
+++ pages/user/user.js
@@ -0,0 +1,15 @@
+// user.js
+const BASE_URL = ""
+const API_BASE_URL = BASE_URL + "/api/v1"
+const FAKE = 0
+const NOT_SURE = 1
+const TRUE = 2
+
+Page({
+  data: {
+
+  },
+  onLoad: function () {
+
+  }
+})
